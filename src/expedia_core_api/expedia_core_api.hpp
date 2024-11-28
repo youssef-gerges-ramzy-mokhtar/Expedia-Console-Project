94f28c904f5ce7d52e428823af95a8c97af63251
youssef-gerges-ramzy-mokhtar <youssef.gerges1@outlook.com>
2024-11-28 07:19:00 +0100

    draft implementation of the expedia booking api logic

--- src/expedia_core_api/expedia_core_api.hpp
+++ src/expedia_core_api/expedia_core_api.hpp
@@ -3,6 +3,9 @@ using namespace std;
 
 #include "../internal_api_wrappers/internal_hotel_apis.hpp"
 #include "../internal_api_wrappers/internal_flight_apis.hpp"
+#include "../internal_api_wrappers/internal_payment_apis.hpp"
+#include "../user_authentication.hpp"
+#include "../expedia_reservation_logic/expedia_reservation_logic.hpp"
 
 #ifndef EXPEDIA_CORE_API_HPP_
 #define EXPEDIA_CORE_API_HPP_
@@ -52,6 +55,22 @@ public:
 		}
 
 		return searchResutls;
+	}
+};
+
+class ExpediaBookingAPI {
+public:
+	bool book(Itinerary itinerary, PaymentInfo paymentInfo, UserInfo userInfo) {
+		// first try to reserve the itinerary
+		// if reservation not succesfull return false
+
+		// if reservation succesffully try payment
+		// if payment failed
+			// cancel the itinerary reservation and return flase
+				// what about the cancellation didn't succeed ????
+
+		// if payment success then register this itinery to the user account and return true
+		return false; // dummy
 	}
 };
 
