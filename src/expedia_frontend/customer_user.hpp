4e877a56af5f2adffa4e965a44d1283002ef2535
youssef-gerges-ramzy-mokhtar <youssef.gerges1@outlook.com>
2024-12-02 07:47:02 +0100

    implemented the UI for creating itineraries, but still the CustomerUserUI not fully implemented

--- src/expedia_frontend/customer_user.hpp
+++ src/expedia_frontend/customer_user.hpp
@@ -2,14 +2,157 @@
 using namespace std;
 
 #include "../expedia_core_api/expedia_booking_api.hpp"
+#include "../expedia_core_api/expedia_flight_api.hpp"
+#include "../expedia_core_api/expedia_hotel_api.hpp"
+#include "../expedia_reservation_logic/flight_reservation.hpp"
+#include "../expedia_reservation_logic/hotel_reservation.hpp"
 #include "user.hpp"
 
 #ifndef CUSTOMER_USER_UI_HPP_
 #define CUSTOMER_USER_UI_HPP_
 
+class ItineraryMakerUI {
+private:
+	ExpediaFlightAPI flightAPI;
+	ExpediaHotelAPI hotelAPI;
+
+private:
+	FlightReservationItem* addFlight() {
+		string from;
+		cout << "Enter From: ";
+		cin >> from;
+
+		string to;
+		cout << "Enter To: ";
+		cin >> to;
+
+		string fromDate;
+		cout << "Enter From Date (dd-mm-yy): ";
+		cin >> fromDate;
+
+		string toDate;
+		cout << "Enter To Date (dd-mm-yy): ";
+		cin >> toDate;
+
+		int adults, children, infants;
+		cout << "Enter # of adults children (5-16) and infants: ";
+		cin >> adults >> children >> infants;
+
+		FlightRequest flightRequest = {fromDate, toDate, from, to, adults, children, infants};
+		vector<FlightInfo> searchResults = flightAPI.search(flightRequest);
+
+		for (int i = 0; i < searchResults.size(); i++) {
+			const auto &flight = searchResults[i];
+			cout << i + 1 << "-"
+				<< " Airline: " << flight.getAirlineName()
+				<< " Cost: " << flight.getPrice()
+				<< " Departure Date: " << flight.getDepartureDateTime()
+				<< " Arrival Date: " << flight.getArrivalDateTime() << "\n";
+		}
+
+		int choice;
+		cout << "Enter -1 to cancel or number in range 1 - " << searchResults.size() << ": ";
+		cin >> choice;
+
+		if (choice == -1)
+			return nullptr;
+
+		assert(1 <= choice && choice <= searchResults.size());
+		return new FlightReservationItem(searchResults[choice -1], flightRequest);
+	}
+
+	HotelReservationItem* addHotel() {
+		string fromDate;
+		cout << "Enter From Date (dd-mm-yy): ";
+		cin >> fromDate;
+
+		string toDate;
+		cout << "Enter To Date (dd-mm-yy): ";
+		cin >> toDate;
+
+		string country;
+		cout << "Enter Country: ";
+		cin >> country;
+
+		string city;
+		cout << "Enter City: ";
+		cin >> city;
+
+		int neededRooms;
+		cout << "Enter number of needed rooms: ";
+		cin >> neededRooms;
+
+		int adults, children;
+		cout << "Enter # of adults and childrne: ";
+		cin >> adults >> children;
+
+		HotelRequest hotelRequest = {fromDate, toDate, country, city, adults, children, neededRooms};
+		vector<HotelRoomInfo> searchResults = hotelAPI.search(hotelRequest);
+
+		for (int i = 0; i < searchResults.size(); i++) {
+			const auto &hotel = searchResults[i];
+			cout << i + 1 << "-"
+				<< " Hotel: " << hotel.getHotelName()
+				<< " Room Type: " << hotel.getRoomType() << "(" << hotel.getAvailableRooms() << ")"
+				<< " Price per night: " << hotel.getPricePerNight()
+				<< " From Date: " << hotel.getFromDate()
+				<< " to " << hotel.getToDate() << "\n";
+		}
+
+		int choice;
+		cout << "Enter -1 to cancel or number in range 1 - " << searchResults.size() << ": ";
+		cin >> choice;
+
+		if (choice == -1)
+			return nullptr;
+
+		assert(1 <= choice && choice <= searchResults.size());
+		return new HotelReservationItem(searchResults[choice -1], hotelRequest);
+	}
+
+public:
+	Itinerary createItinerary() {
+		Itinerary itinerary;
+
+		while (true) {
+			int choice = readMenuOption({"Add Flight", "Add Hotel", "Done", "Cancel"});
+
+			IReservationItem* item = nullptr;
+			if (choice == 1)
+				item = addFlight();
+			else if (choice == 2)
+				item = addHotel();
+			else if (choice == 3)
+				break;
+			else if (choice == 4)
+				return Itinerary(); // empty Itinerary
+
+			if (item != nullptr)
+				itinerary.addReservationItem(item);
+		}
+
+		return itinerary;
+	}
+};
+
 class CustomerUserUI: public UserUI {
 private:
 	ExpediaBookingAPI expediaBookingAPI;
+	ItineraryMakerUI itineraryMakerUI;
+
+private:
+	// still under implementation
+	void makeItinerary() {
+		Itinerary itinerary = itineraryMakerUI.createItinerary();
+		if (itinerary.getAllReservation().size() == 0)
+			return;
+	}
+
+	void listUserItineraries() {
+		vector<Itinerary> itineraries = expediaBookingAPI.getUserBookedItineraries(getUserInfo());
+		for (auto &itinerary: itineraries)
+			cout << itinerary.toString() << "\n";
+	}
 
 public:
 	CustomerUserUI(const UserInfo &userInfo) : UserUI(userInfo) {
@@ -18,7 +161,18 @@ public:
 	}
 
 	virtual void runUI() override {
-		cout << "Customer User still under development\n";
+		cout << "Hello " << getUserInfo().getUserId() << " | Customer View\n\n";
+		while (true) {
+			int choice = readMenuOption({"View Profile", "Make Itinerary", "List my itineraries", "Logout"});
+			if (choice == 1)
+				viewProfile();
+			else if (choice == 2)
+				makeItinerary();
+			else if (choice == 3)
+				listUserItineraries();
+			else if (choice == 4)
+				break;
+		}
 	}
 };
 
