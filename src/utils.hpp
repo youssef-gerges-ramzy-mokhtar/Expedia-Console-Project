7e7cf230323172d5eeeb10046d3f258c4a6ead62
youssef-gerges-ramzy-mokhtar <youssef.gerges1@outlook.com>
2024-12-02 06:41:27 +0100

    adding a helper function to be used by frontned code to read menu options

--- src/utils.hpp
+++ src/utils.hpp
@@ -1,5 +1,7 @@
 #include <string>
+#include <vector>
 #include <iostream>
+#include <cassert>
 using namespace std;
 
 #ifndef UTILS_H_
@@ -11,6 +13,21 @@ bool containSpaces(const string &str) {
 			return true;
 
 	return false;
+}
+
+int readMenuOption(const vector<string> &menuOptions) {
+	cout << "Menu:\n";
+	for (int i = 0; i < menuOptions.size(); i++)
+		cout << "\t" << i+1 << ". " << menuOptions[i] << "\n";
+	
+	cout << "\n";
+
+	int choice;
+	cout << "Enter number in range 1 - " << menuOptions.size() << ": ";
+	cin >> choice;
+
+	assert(1 <= choice && choice <= menuOptions.size());
+	return choice;
 }
 
 #endif
