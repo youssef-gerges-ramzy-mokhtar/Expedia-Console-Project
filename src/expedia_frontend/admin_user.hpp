973481d547cab9250a1e94f31dbc17ba662364e1
youssef-gerges-ramzy-mokhtar <youssef.gerges1@outlook.com>
2024-12-02 06:43:55 +0100

    checking for the user type in the AdminUser constructor

--- src/expedia_frontend/admin_user.hpp
+++ src/expedia_frontend/admin_user.hpp
@@ -5,7 +5,10 @@
 
 class AdminUserUI: public UserUI {
 public:
-	AdminUserUI(UserInfo userInfo) : UserUI(userInfo) {}
+	AdminUserUI(const UserInfo &userInfo) : UserUI(userInfo) {
+		if (userInfo.getUserType() != UserType::ADMIN)
+			throw invalid_argument("Invalid User Type, expected ADMIN user");
+	}
 
 	virtual void runUI() override {
 		cout << "Admin User doesn't have any operations at the moment\n";
